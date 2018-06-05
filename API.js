c54e4995a1e2a751c3b53c18801376f9339d576f
FireInjun <Fireinjun291@gmail.com>
2018-06-05 14:23:31 -0400

    more changes
    still working

--- API.js
+++ API.js
@@ -1,201 +1,35 @@
-//const axios = require( 'axios' );
-const server = require( './server' );
 const db = require( './data/db' );
 
 class API
 {
-
     constructor( { url } )
     {
         this.url = url
         this.endpoints = {}
     }
-    /**
-     * Create and store a single entity's endpoints
-     * @param {A entity Object} entity
-     */
+
     createEntity( entity )
     {
-        this.endpoints[ entity.name ] = this.createBasicCRUDEndpoints( entity )
+        this.endpoints[ entity ] = this.createBasicCRUDEndpoints( entity )
     }
 
     createEntities( arrayOfEntity )
     {
-        arrayOfEntity.forEach( this.createEntity.bind( this ) )
-    }
-    /**
-     * Create the basic endpoints handlers for CRUD operations
-     * @param {A entity Object} entity
-     */
-    createBasicCRUDEndpoints( { name } )
-    {
-        var endpoints = {}
-
-        const resourceURL = `${ this.url }/${ name }`
-
-        endpoints.getAll = ( { query } = {} ) => axios.get( resourceURL, { params: { query } } )
-
-        endpoints.getOne = ( { id } ) => axios.get( `${ resourceURL }/${ id }` )
-
-        endpoints.create = ( toCreate ) => axios.post( resourceURL, toCreate )
-
-        endpoints.update = ( toUpdate ) => axios.put( `${ resourceURL }/${ toUpdate.id }`, toUpdate )
-
-        endpoints.patch = ( { id }, toPatch ) => axios.patch( `${ resourceURL }/${ id }`, toPatch )
-
-        endpoints.delete = ( { id } ) => axios.delete( `${ resourceURL }/${ id }` )
-
-        return endpoints
-
+        arrayOfEntity.forEach( each => this.createEntity( each ) )
     }
 
-}
-
-export default API
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
-//This is my API file, I have it separated from the server.js file so that I can keep my code cleaner and simplify my calls.
-
-
-// Named methods on db.js
-// find,
-// findById,
-// insert,
-// update,
-// remove.
-
-
-
-
-class API
-{
-
-    constructor( { url } )
+    createBasicCRUDEndpoints( name )
     {
-        this.url = "/api/users"
-        this.endpoints = {}
-    }
-    // Create and store a single entity's endpoints
-    //@param {A user Object} user
-    createUser( user )
-    {
-        this.endpoints[ user.name ] = this.createBasicCRUDEndpoints( user )
-    }
+        let endpoints = {}
+        const resourceURL = `${ this.url }/${ name }}`
 
-    createUsers( users )
-    {
-        users.forEach( this.createUser.bind( this ) )
-    }
-
-    //Create the basic endpoints handlers for CRUD operations
-    // @param {A user Object} user
-
-    createBasicCRUDEndpoints( { name } )
-    {
-        var endpoints = {}
-
-        const resourceURL = `${ this.url }/${ name }`
-
-        // find (all)
-        endpoints.find = ( { query } = {} ) => db.find()
-
-
-        // findById
-        endpoints.findById = ( { id } ) =>
-            server.get( `${ resourceURL }/${ id }`, ( req, res ) =>
-            {
-                const { id } = req.params;
-                const { name, bio } = req.body;
-                db
-                    .findById( { id } )
-                    .then( users =>
-                    {
-                        res.json( { users: id } );
-                    } )
-                    .catch( error =>
-                    {
-                        res.json( { error } );
-                    } );
-            } )
-
-        // insert
-        endpoints.insert = ( toInsert ) =>
-            server.post( resourceURL, ( req, res ) =>
-            {
-                const { name, bio } = req.body;
-                db
-                    .insert( { name, bio } )
-                    .then( response =>
-                    {
-                        res.json( response );
-                    } )
-                    .catch( error =>
-                    {
-                        console.log( error )
-                        res.json( error );
-                    } );
-            } )
-
-        // update
-        endpoints.update = ( toUpdate ) =>
-            server.put( `${ resourceURL }/${ update.id }`, ( req, res ) =>
-            {
-                const { id } = req.params;
-                const { name, bio } = req.body;
-                const selectedUserForUpdate = user =>
-                {
-                    return
-                    db
-                        .findById( { id } ) == id;
-                };
-                if ( !selectedUserForUpdate )
-                {
-                    return sendUserError( 'No user to edit!'.res );
-                } else
-                {
-                    if ( name ) selectedUserForUpdate.name = name;
-                    if ( bio ) selectedUserForUpdate.bio = bio;
-                    res.json( users );
-                }
-            } )
-
-        // remove
-        endpoints.remove = ( { id } ) =>
-            server.delete( `${ resourceURL }/${ id }`, ( req, res ) =>
-            {
-                const { id } = req.params;
-                const { name, bio } = req.body;
-                const selectedUserforRemoval = user =>
-                {
-                    return
-                    db
-                        .findById( { id } ) == id;
-                };
-
-                if ( selectedUserforRemoval )
-                {
-                    const userRemoved = { ...selectedUserforRemoval };
-                    users = users.filter( user => users.id != id );
-                    res.status( 200 ).json( users );
-                } else
-                {
-                    sendUserError( "No user matches that ID to delete! You probably ran them off already...", res );
-                }
-            } )
-
+        endpoints.getAll = () => db( `${ name }` )
+        endpoints.getOne = ( { id } ) => db( `${ name }` ).where( { id: Number( id ) } )
+        endpoints.insert = ( { data } ) => db( `${ name }` ).insert( data )
+        endpoints.update = ( { id, data } ) => db( `${ name }` ).where( 'id', Number( id ) ).update( data )
+        endpoints.remove = ( { id } ) => db( `${ name }` ).where( 'id', Number( id ) ).del()
         return endpoints
     }
-}
+};
 
-module.exports = API
+module.exports = API;
--- server.js
+++ server.js
@@ -1,36 +1,138 @@
 const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 const db = require( './data/db' );
+const CORS = require( 'cors' );
+const API = require( './userApi.js' );
+
 const port = 5000;
 const server = express();
+
+
 server.use( express.json() );
-
-
-const Users = new API( { url: `http://localhost:${ port }/api` } )
-server.listen( port, () => console.log( `Server running on port ${ port }. Your API is located here at ${ URL }.` ) );
-// const express = require( 'express' );
-// const db = require( './data/db' )
-// const API = require( './api' )
-// const server = express();
-// server.use( express.json() )
-// const port = 5000;
-
-// const myApi = new API( { url: 'http://localhost:5000/api' } )
-// myApi.createUser( { name: 'users' } )
-
-// server.get( myApi.url, ( _, res ) =>
-// {
-//     myApi.endpoints.users.find()
-//         .then( response =>
-//         {
-//             res.status( 200 ).json( response )
-//         } )
-//         .catch( err => console.log( err ) )
-//} )
-//
-// myApi.endpoints.users.findById("whatever")
-//     .then(res => res.status(200).json(res))
-//     .catch(err => console.log(err))
+server.use( bodyParser.json() );
+server.use( bodyParser.urlencoded({extended: true}) );
+server.use( CORS() );
 
 
 
-// server.listen( port, () => console.log( `Server running on port ${ port }.` ) )
+const userApi = new API( { url: `http://localhost:5000/api` } )
+const entities = [ 'users' ]
+const requiredFields = { 'users': [ 'name', 'bio' ] }
+userApi.createEntities(entities)
+
+entities.forEach( each =>
+{
+    server.get( `/api/${ each }`, ( _, res ) =>
+    {
+        api.endpoints[ each ].getAll()
+            .then( response => res.status( 200 ).json( response ) )
+            .catch(err => res.status(500).json({errorMessage: `The ${each} information could not be retrieved.`}))
+    } )
+    server.get( `/api/${ each }/:id`, ( req, res ) =>
+    {
+        const { id } = req.params
+        api.endpoints[ each ].getOne( { id } )
+            .then( response =>
+            {
+                if ( response.length > 0 )
+                {
+                    res.status( 200 ).send( response )
+                } else
+                {
+                    res.status( 404 ).json( { errorMessage: `The ${ each } with the specified ID does not exist.` } )
+                }
+            } )
+            .catch( error =>
+            {
+                res.status( 500 ).json( { errorMessage: `The ${ each } information could not be retrieved.` } )
+            } )
+    } )
+
+    // Insert one object to a specific entity
+    server.post( `/api/${ each }`, ( req, res ) =>
+    {
+        const data = req.body
+        for ( let i = 0; i < requiredFields[ each ].length; i++ )
+        {
+            const field = requiredFields[ each ][ i ]
+            if ( !req.body[ field ] )
+            {
+                res.status( 400 ).json( { errorMessage: `Please provide ${ field } for the ${ each }.` } );
+                return;
+            }
+        }
+
+        userApi.endpoints[ each ].insert( { data } )
+            .then( response =>
+            {
+                res.status( 200 ).json( { id: response[ 0 ], ...data } )
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { errorMessage: `The ${ each } information could not be retrieved.` } )
+            } )
+    } )
+
+    // Delete one object of a specific entity
+    server.delete( `/api/${ each }/:id`, ( req, res ) =>
+    {
+        const { id } = req.params
+        userApi.endpoints[ each ].remove( { id } )
+            .then( response =>
+            {
+                if ( response === 1 )
+                {
+                    res.status( 200 ).send( `The ${ each } have been deleted` )
+                } else
+                {
+                    res.status( 404 ).json( { errorMessage: `The ${ each } with the specified ID does not exist.` } )
+                }
+            } )
+            .catch( error =>
+            {
+                res.status( 500 ).json( { errorMessage: `The ${ each } information could not be retrieved.` } )
+            } )
+    } )
+
+    // Update one object of a specific entity
+    server.put( `/api/${ each }/:id`, ( req, res ) =>
+    {
+        const data = req.body
+        const { id } = req.params
+
+        // check if updated object contains all required fields
+        for ( let i = 0; i < requiredFields[ each ].length; i++ )
+        {
+            const field = requiredFields[ each ][ i ]
+            if ( !req.body[ field ] )
+            {
+                res.status( 400 ).json( { errorMessage: `Please provide ${ field } for the ${ each }.` } );
+                return;
+            }
+        }
+
+        // for other entities
+        userApi.endpoints[ each ].update( { id, data } )
+            .then( response =>
+            {
+                if ( response === 1 )
+                {
+                    res.status( 200 ).send( `The ${ each } have been updated` )
+                } else
+                {
+                    res.status( 404 ).json( { errorMessage: `The ${ each } with the specified ID does not exist.` } )
+                }
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { errorMessage: `The ${ each } information could not be retrieved.` } )
+            } )
+    } )
+
+} )
+
+
+server.listen( port, () =>
+{
+    console.log( `Server listening on port ${ port }` );
+} )
